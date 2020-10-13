ca136c55ce3b0722c4e4f16d17c7b1c124df0634
unknown <mittalshubham05@gmail.com>
2020-10-13 13:44:22 +0530

    Create: created EmployeePayrollService, input and output from/to console

---
+++ src/main/java/com/bridgelabz/fileio/Employee.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.fileio;
+
+/**
+ * Hello world!
+ *
+ */
+public class Employee {
+	private int employeeId;
+	private String name;
+	private double salary;
+
+	/**
+	 * Parameterized Constructor
+	 */
+	public Employee(int employeeId, String name, double salary) {
+		super();
+		this.employeeId = employeeId;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	/**
+	 * Prints details of Employee on console
+	 */
+	@Override
+	public String toString() {
+		return ("The employee details are as follows:\nEmployeeID: " + employeeId + "\nName: " + name + "\nSalary: "
+				+ salary + "\n");
+	}
+}
